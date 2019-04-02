df64ddde6c796545e1d7941621f3ac90874702ce
Emilie Bialais <ebialais@gmail.com>
2019-04-02 15:12:24 +0200

    feature_Front deplacment formu dans infos

--- vue/src/views/Infos/Infos.vue
+++ vue/src/views/Infos/Infos.vue
@@ -5,8 +5,28 @@
 </template>
 
 <script>
+    import formu from './components/Infos/formu.vue';
+
     export default {
         name: 'infos',
+        components: {
+            formu, 
+        },
+        beforeMount: function() {
+            this.getData()
+        },
+        methods:{
+            getData(){
+                const req = new XMLHttpRequest();
+                req.open('GET','http://localhost:8888/Projet_allezcine/allezcine/php/getData.php', false);
+                req.send(null);
+                if (req.status === 200 ){
+                    this.lists = JSON.parse(req.response)
+                } else {
+                }
+            },
+        },
+
     }
 </script>
 
