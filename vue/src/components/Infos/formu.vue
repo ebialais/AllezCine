ee29fba30328aef914eadd8d46119a2dd417e46c
Pierre-Louis Picard <Pierre-Louis@Ioanas-MacBook-Air.local>
2019-04-02 14:46:58 +0200

    Added Formu

---
+++ vue/src/components/Infos/formu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="formu" @submit="handleSubmit">
+        <form>
+            <p>Comments : </p>
+            <input name="userCom" type="text">
+            <br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"formu",
+    methods: {
+        handleSubmit(e){
+            e.preventDefault() 
+            let userCom = e.target.userCom.value;
+            // console.log(userCom);
+            this.sendData(userCom); 
+        },
+        sendData(title){ 
+            const req = new XMLHttpRequest();
+            let query = `userCom=${title}` 
+            // console.log(query)
+            req.open('POST',`http://localhost:8888/Projet_allezcine/allezcine/php/getData.php?${query}`, false);
+            req.send();
+            if (req.status === 200 ){
+                // console.log(req.response)
+                this.getData()
+            } else {
+                // console.log('error', req.status, req.statusText)
+            }
+        }
+    },
+    props:[
+        "getData",
+    ]
+}
+</script>
+<style>
+</style>
